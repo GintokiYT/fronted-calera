f7364adc617220f936d6b118d01a2578323018e9
Renato <satoshiune21@gmail.com>
2025-06-01 00:47:19 -0500

    first commit v1

--- src/pages/dashboard/equipos/page.tsx
+++ src/pages/dashboard/equipos/page.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react"
+
 export default function Page() {
+
+  const [ state, setState ] = useState("");
+
   return (
-    <div>Equipos</div>
+    <div>
+      <h1>Equipos</h1>
+      <select name="" id="" value={state} onChange={(e) => setState(e.target.value)}>
+        <option value="">Seleccione un equipo</option>
+        <option value="laptop">Laptop</option>
+        <option value="impresora">Impresora</option>
+      </select>
+
+      { state === "" ? <h1>No ha seleccionado una opcion aun</h1> : <h1>Haz seleccionado { state }</h1>}
+    </div>
   )
 }
