380f5641d3fb04f6823fe516e70d21bfb4b8648c
Renato <satoshiune21@gmail.com>
2025-06-02 23:26:23 -0500

    lobito masca gampi

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
-    host: true
+    host: true,
+    allowedHosts: true
   },
   resolve: {
     alias: {
